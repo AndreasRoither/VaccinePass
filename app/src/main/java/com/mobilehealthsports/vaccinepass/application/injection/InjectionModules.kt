f0d00d597f02c1bf217bedb023c7f88b88f2c621
Andreas Roither <andi.roither@protonmail.com>
2020-10-14 20:54:17 +0200

    Update injection module

--- app/src/main/java/com/mobilehealthsports/vaccinepass/application/injection/InjectionModules.kt
+++ app/src/main/java/com/mobilehealthsports/vaccinepass/application/injection/InjectionModules.kt
@@ -1,9 +1,20 @@
 package com.mobilehealthsports.vaccinepass.application.injection
 
+import android.app.Application
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import org.koin.dsl.module
 
 object InjectionModules {
     fun getModules() = listOf(
+
+        // AppModule
+        module {
+            single { get<Application>().getSharedPreferences("appPreferences", 0) }
+            factory<Gson> { GsonBuilder().create() }
+        },
+
+        // ViewModel module
         module {
             // androidx.lifecycle.viewmodel
             // viewModel { My´ViewModel(get(), get())}
