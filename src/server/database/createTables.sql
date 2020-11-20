9959fa6a83da3a5df93e5c42c799f17d1d0cca52
MarkusSeiberl <markus_seiberl@live.at>
2020-11-20 19:04:43 +0100

    Adds docker support; Updates documentation

---
+++ src/server/database/createTables.sql
@@ -0,0 +1,28 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+	user_id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+	name VARCHAR(25) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+	password VARCHAR(60) NOT NULL,
+    mail VARCHAR(50) NOT NULL,
+    birth DATE NOT NULL,
+    weight decimal NOT NULL,
+    height smallint NOT NULL,
+    bloodtype VARCHAR(10) NOT NULL,
+    maindoctor VARCHAR(50),
+    UNIQUE(mail)
+);
+
+CREATE TABLE IF NOT EXISTS vaccinations (
+	vaccination_id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    name VARCHAR(50) NOT NULL,
+    user_id uuid NOT NULL,
+    vaccination_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) 
+        REFERENCES users(user_id)
+);
+
+
--- src/server/restAPI/src/index.js
+++ src/server/restAPI/src/index.js
@@ -3,14 +3,15 @@ const app = express()
 const { Client } = require('pg');
 
 
-var connectionString = "postgres://admin:123@localhost:5432/postgres";
+var connectionString = "postgres://admin:123@database:5432/postgres";
 
 const client = new Client({
     connectionString: connectionString
 });
+client.connect();
 
 
-app.get('/', (req, res) => res.send('Hello world asdfasdf'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 
 app.get('/register', (req, res) => {
@@ -21,10 +22,6 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
 
 });
-
-
-//app.get('/changepw')
-//app.get('/updateuserinfo')
 
 
 
